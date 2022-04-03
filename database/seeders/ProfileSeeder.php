05c7b33a45776ff0abffa3672cd080e0d784a085
Muhammad Ikhwan Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2022-04-04 05:34:06 +0700

    deploy

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -18,21 +18,21 @@ class ProfileSeeder extends Seeder
         DB::table('profiles')->insert([
             [
                 'profile_id' => '1',
-                'id' => '1',
+                'user_id' => '1',
                 'age' => '20',
                 'bio' => 'Jabarcoding Camp',
                 'address' => 'Bandung',
             ],
             [
                 'profile_id' => '2',
-                'id' => '2',
+                'user_id' => '2',
                 'age' => '20',
                 'bio' => 'Jabarcoding Camp',
                 'address' => 'Bandung',
             ],
             [
                 'profile_id' => '3',
-                'id' => '3',
+                'user_id' => '3',
                 'age' => '20',
                 'bio' => 'Jabarcoding Camp',
                 'address' => 'Bandung',
